e75b1514e0427c6fce1223ed9afc505d442e84ef
Jaleeh <jasonleeho990@gmail.com>
2023-02-17 15:23:13 +0000

    cleanup CRUD

--- secure_notes_api/api/urls.py
+++ secure_notes_api/api/urls.py
@@ -1,19 +1,31 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import NoteViewSet,SingleNoteView,NoteDeleteView,NoteCreateView
+# from .views import NoteViewSet,SingleNoteView,NoteDeleteView,NoteCreateView
+from .views import NoteViewSet
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Note
+from .serializers import NoteSerializer
 
+@api_view(['GET','POST'])
+def my_notes(request):
+    notes = Note.objects.filter(name=request.user.id)
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
 
 router = DefaultRouter()
-router.register(r'notes', NoteViewSet)
-
+router.register(r'notes', NoteViewSet,basename = 'note')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('notes/<int:pk>/',SingleNoteView.as_view(),name = 'note'), #gets note by id
-    path('notes/<int:pk>/delete',NoteDeleteView.as_view(),name = 'delete_note'), #deletes note by id
-    path('notes/create', NoteCreateView.as_view(), name='note-create'),
-
-
+    # path('notes/<int:pk>/',SingleNoteView.as_view(),name = 'note'), #gets note by id
+    # path('notes/<int:pk>/delete',NoteDeleteView.as_view(),name = 'delete_note'), #deletes note by id
+    # path('notes/create', NoteCreateView.as_view(), name='note-create'),
+    
+    path('notes/<int:pk>/', NoteViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy','post': 'perform_create'}), name='single-note'),
+    path('mynotes/',my_notes),
+    
 
 ]
+urlpatterns+=router.urls
--- secure_notes_api/api/views.py
+++ secure_notes_api/api/views.py
@@ -1,9 +1,11 @@
 
-from rest_framework import viewsets,permissions,response,generics
+from rest_framework import viewsets,permissions,response,generics,status
 from rest_framework.generics import RetrieveAPIView
 from .models import Note
 from .serializers import NoteSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.http import Http404
+
 
 #view all notes
 class NoteViewSet(viewsets.ModelViewSet):
@@ -18,26 +20,53 @@ class NoteViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_excepton = True)
         self.perform_update(serializer)
         return response.Response(serializer.data)
-
-#view individual note
-class SingleNoteView(RetrieveAPIView):
-    authentication_classes = (JWTAuthentication,)
-    permission_classes = (permissions.IsAuthenticated,)
-    serializer_class = NoteSerializer
-    def get_queryset(self):
-        return Note.objects.filter(id=self.kwargs.get(self.lookup_field))#filter based on ID
-
-class NoteDeleteView(generics.DestroyAPIView): 
-    authentication_classes = (JWTAuthentication,)
-    permission_classes = (permissions.IsAuthenticated,)
-    queryset = Note.objects.all() 
-    serializer_class = NoteSerializer
-
-class NoteCreateView(generics.CreateAPIView):
-    authentication_classes = (JWTAuthentication,)
-    permission_classes = (permissions.IsAuthenticated,)
-    queryset = Note.objects.all()
-    serializer_class = NoteSerializer
-
+    
     def perform_create(self, serializer):
-        return serializer.save(user=self.request.user)
+        return serializer.save(name=self.request.user)
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return response.Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def retrieve(self, request, pk = None):
+        user = request.user
+        try:
+            note = Note.objects.get(pk=pk,name=user)
+        except Note.DoesNotExist:
+            raise Http404
+        serializer = self.get_serializer(note)
+        return response.Response(serializer.data)
+        
+
+# #view individual note
+# class SingleNoteView(RetrieveAPIView):
+#     authentication_classes = (JWTAuthentication,)
+#     permission_classes = (permissions.IsAuthenticated,)
+#     serializer_class = NoteSerializer
+#     def get_queryset(self):
+#         return Note.objects.filter(id=self.kwargs.get(self.lookup_field))#filter based on ID
+
+# class NoteDeleteView(generics.DestroyAPIView): 
+#     authentication_classes = (JWTAuthentication,)
+#     permission_classes = (permissions.IsAuthenticated,)
+#     queryset = Note.objects.all() 
+#     serializer_class = NoteSerializer
+
+#     def delete(self,request,pk):
+#         try:
+#             note = Note.objects.get(pk=pk,user = request.user) # check note exists in user account
+#         except Note.DoesNotExist:
+#             return response.Response(status= status.HTTP_404_NOT_FOUND)
+#         note.delete() #if True then delete note
+#         return response.Response(status= status.HTTP_204_NO_CONTENT) 
+
+
+# class NoteCreateView(generics.CreateAPIView):
+#     authentication_classes = (JWTAuthentication,)
+#     permission_classes = (permissions.IsAuthenticated,)
+#     queryset = Note.objects.all()
+#     serializer_class = NoteSerializer
+
+#     def perform_create(self, serializer):
+#         return serializer.save(user=self.request.user)
